c145ab032fd7ba8920c25a43bbf26a08b550a310
QI117 <114166587+QI117@users.noreply.github.com>
2022-11-17 18:10:12 +0000

    cholesky

--- imran.r
+++ imran.r
@@ -4,12 +4,6 @@ rb <- function(th,k=2) {
   k*(th[2]-(th[1])^2)^2 + (1-th[1])^2 
 }
 
-theta <- c(35, 35)
-
-f <- rb(theta)
-h<- hb(theta)
-g <- gb(theta)
-
 gb <- function(th,k=2) {
   c(-2*(1-th[1])-k*4*th[1]*(th[2]-th[1]^2),k*2*(th[2]-th[1]^2))
 }
@@ -46,33 +40,34 @@ newt <- function(theta,func,grad,hess=NULL,...,tol=1e-8,fscale=1,maxit=100,max.h
       }
     }
     else {h <- hess(theta)}
-    hf <- h
+    
     R <- try(chol(h), silent = TRUE) # try to decompose the matrix by Cholesky decomposition
     if(inherits(R, "try-error")) # if decomposition fails 
     {
       multiplier <- norm(h)*1e-6
-      R <- try(chol(h))
       while(inherits(R, "try-error")) # if it fails again
       {
-        
       #while(sum(eigen(h)$values>=0) != length(theta)) {
         h <- h + diag(length(theta))*multiplier
-        multiplier <- 10*multiplier 
         R <- try(chol(h))
+        multiplier <- 10*multiplier 
       }
     }
     
-    delta <- -ginv(h)%*%g
+    delta <- backsolve(R, forwardsolve(t(R), -g))
     if (sum(abs(g) < tol*(abs(f) + fscale)) == length(theta)) {break} 
     
     if (func(theta + delta,...) >= f) {
       for(j in 1:max.half) {
         delta <- delta/2
-        if(func(theta + delta,...) < f) {break}
+        if(func(theta + delta,...) < f & all(is.finite(func(theta+delta))) == TRUE) {break}
       }
     }
     if (func(theta + delta,...) >= f) {warning("max.half is reached without convergence.")
       break}
+    if(all(is.finite(func(theta+delta))) == FALSE){
+      warning("max.half is reached with the trial objective function not finite", call. = FALSE)}
+    
     theta <- theta + delta
     iter <- iter+1
   }
@@ -81,17 +76,19 @@ newt <- function(theta,func,grad,hess=NULL,...,tol=1e-8,fscale=1,maxit=100,max.h
     warning("maxit is reached without convergence.")
   } 
   
-  R <- try(chol(hf), silent = TRUE) # *** try to decompose the matrix by Cholesky decomposition
-  if(inherits(R, "try-error")) # if decomposition fails
-  {
-    warning("The hessian at convergence is not positive definite.")
-    Hi <- NULL}
+  if (!inherits(try(chol(h), silent = TRUE), "try-error")) {
+    Hi <- backsolve(R, forwardsolve(t(R), diag(length(theta))))
+    
+  }
+  else{
+    Hi <- NULL
+    warning("Hessian matrix is not positive definite", call. = FALSE)
+  }
   
-  else {Hi <- ginv(hf)}
   return(list("f"=f,"theta"=theta, "g"=g, "iter"=iter, "Hi"=Hi))
-  
 }
 
+theta <- c(200,200)
 n1 <- newt(theta,rb,gb,hess=hb,tol=1e-8,fscale=1,maxit=100,max.half=20,eps=1e-6)
 n2 <- newt(theta,rb,gb,hess=NULL,tol=1e-8,fscale=1,maxit=100,max.half=20,eps=1e-6)
 system.time(newt(theta,rb,gb,hess=hb,tol=1e-8,fscale=1,maxit=100,max.half=20,eps=1e-6))
